ce783918f4f21c326794193114784db651963215
James <winleiei@Wins-MacBook-Air.local>
2024-09-02 19:32:57 +0630

    Added Sidebars & avatars section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,19 @@
 "use client";
 import Live from "@/components/Live";
 import { CollaborativeApp } from "./CollaborativeApp";
+import Navbar from "@/components/Navbar";
+import LeftSidebar from "@/components/LeftSidebar";
+import RightSidebar from "@/components/RightSidebar";
 
 export default function Page() {
   return (
-    <div>
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <LeftSidebar />
+        <Live />
+        <RightSidebar />
+      </section>
+    </main>
   );
 }
